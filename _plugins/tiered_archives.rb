5ffa2b8400d333fddb9996fcea2f78d59bdaefbf
Eli Naeher <enaeher@gmail.com>
2011-09-05 16:48:31 -0500

    add some docs to tiered archives plugin

--- _plugins/tiered_archives.rb
+++ _plugins/tiered_archives.rb
@@ -1,4 +1,19 @@
+# A plugin for Jekyll by Eli Naeher
+#
+# This plugin creates a site.years template variable which allow you to group archive links by year and month.
+# The structure of site.years is:
 # site.years = 2001=>[1=>[post1, post2...], 2=>[...]], 2002=>[...]
+#
+# Usage should look something like this:
+# {% for year in site.years %}
+#   <h2>Year {{ year.first.first.date | date: "%Y" }}</h2>
+#   {% for month in year %}
+#     <h3>Month {{ month.first.date | date: "%B" }}</h3>
+#     {% for post in month %}
+#       <a href="{{ post.url">{{ post.title }}</a>
+#     {% endfor %}
+#   {% endfor %}
+# {% endfor %}
 
 class Jekyll::Site
 	alias :site_payload_without_tiered_archives :site_payload
